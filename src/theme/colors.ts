87826037f203c77854945ebdff1eb8aceeb42fa2
Starsidea <director_9414@outlook.com>
2021-05-07 05:45:40 -0700

    initial commit

---
+++ src/theme/colors.ts
@@ -0,0 +1,55 @@
+import { Colors } from "./types";
+
+export const baseColors = {
+  failure: "#ED4B9E",
+  primary: "#0f84f8",
+  primaryBright: "#0f84f8",
+  primaryDark: "#0f84f8",
+  secondary: "#0f84f8",
+  success: "#31D0AA",
+  warning: "#FFB237",
+};
+
+export const brandColors = {
+  binance: "#F0B90B",
+};
+
+export const lightColors: Colors = {
+  ...baseColors,
+  ...brandColors,
+  background: "#FAF9FA",
+  backgroundDisabled: "#E9EAEB",
+  contrast: "#191326",
+  invertedContrast: "#FFFFFF",
+  input: "#eeeaf4",
+  tertiary: "#EFF4F5",
+  text: "#063767",
+  textDisabled: "#BDC2C4",
+  textSubtle: "#6C9DCD",
+  borderColor: "#E9EAEB",
+  card: "#FFFFFF",
+  gradients: {
+    bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
+  },
+};
+
+export const darkColors: Colors = {
+  ...baseColors,
+  ...brandColors,
+  secondary: "#0F84F8",
+  background: "black",
+  backgroundDisabled: "#3c3742",
+  contrast: "#FFFFFF",
+  invertedContrast: "#191326",
+  input: "#483f5a",
+  primaryDark: "#0f84f8",
+  tertiary: "#353547",
+  text: "#b5cee6",
+  textDisabled: "#666171",
+  textSubtle: "#c9c4d4",
+  borderColor: "#524B63",
+  card: "#27262c",
+  gradients: {
+    bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
+  },
+};
